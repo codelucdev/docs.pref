4cf2ae2a81f936a9db91504db3549c067ecb160f
codelucdev <146024482+codelucdev@users.noreply.github.com>
2024-04-05 14:15:28 -0300

    Update script.js

--- NOTIFICACAO_COPASA/script/script.js
+++ NOTIFICACAO_COPASA/script/script.js
@@ -37,15 +37,15 @@ document.addEventListener('DOMContentLoaded', function() {
                     </header>
 
                     <section style="margin: 0 65px;">
-                        <p>
+                        <p style="text-indent: 3em";>
                             <strong>A SECRETARIA DE OBRAS E SERVIÇOS URBANOS DO MUNICÍPIO DE SÃO JOÃO DA PONTE</strong>, entidade de direito público interno, pertencente ao Estado de Minas Gerais,
                             no CNPJ sob n° 16.928.483/0001-29, sediada na Praça Olímpio Campos, n° 128, Centro, nesta cidade de São João da Ponte, vem perante a Vossa Senhoria
                              promover <strong>NOTIFICAÇÂO PRELIMINAR</strong>, ficando assim a entidade obrigada a recompor a via pública, conforme foto anexa, localizada na ${rua}, próximo 
                              ao imóvel n° ${n_proximo}, ${bairro}, neste município, com o piso existente anteriormente, <strong>no praso máximo de ${prazoDias} dias</strong> a contar do recebimento.
                         </p>
                         
-                        <p>
-                            Saliento que o não cumprimento desta NOTIFICAÇÃO PRELIMINAR no prazo estipulado pela Lei Municipal acarretará MULTA DIÁRIA no valor de 350 UFM (Unidade Fiscal Municipal),
+                        <p style="text-indent: 3em;">
+                                Saliento que o não cumprimento desta NOTIFICAÇÃO PRELIMINAR no prazo estipulado pela Lei Municipal acarretará MULTA DIÁRIA no valor de 350 UFM (Unidade Fiscal Municipal),
                             o que equilave hoje ao valor de R$ 2.086,00 ( dois mil e oitenta e seis reais), que somente cessará a cobrança até que seja restabelecido a via para seu uso regular. 
 
                         </p><br><br>
@@ -53,7 +53,7 @@ document.addEventListener('DOMContentLoaded', function() {
                             <p style="padding-bottom: 30px";><strong>Data de vencimento:</strong> ${new Date(new Date().getTime() + (prazoDias * 24 * 60 * 60 * 1000)).toLocaleDateString()}</p>
                     </section>
 
-                    <footer style="display:flex; justify-content: center; align-items: center; flex-direction: column; heigh: 100%; width: 100%; padding-bottom:260px;" >
+                    <footer style="display:flex; justify-content: center; align-items: center; flex-direction: column; heigh: 100%; width: 100%; padding-bottom:300px;" >
                         
                         <p>_________________________________</p><br>
                         <p style="text-align:center;><strong">${responsavel}</strong></p><br><br>
